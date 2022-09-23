5e4b3fc271e86f4856cfe23ca0bcc1ef07d1d40d
mrustl <michael.rustler@kompetenz-wasser.de>
2022-09-23 12:14:46 +0200

    Remove "ows4R" dependency, switch default srs
    and in addition explicity provide encoding and improve error handling

--- R/read_wfs.R
+++ R/read_wfs.R
@@ -7,13 +7,13 @@
 #' 3. Read the content of \code{Rechneraddresse} in the opened tab. The basename
 #' of the url is the required dataset id!
 #' @param service_version one of "1.0.0", "1.1.0" or "2.0.0" (default: "2.0.0")
-#' @param srs_name one of "EPSG:4258" or "EPSG:25833" (default: "EPSG:4258")
+#' @param srs one of "EPSG:4258" or "EPSG:25833" (default: "EPSG:25833")
+#' @param encoding default: UTF-8
 #' @param debug prints debug messages if TRUE (default: TRUE)
 #' @return imports selected WFS dataset into R
 #' @export
 #' @importFrom fs path_join
 #' @importFrom httr build_url content GET parse_url
-#' @importFrom ows4R WFSClient
 #' @importFrom sf read_sf
 #' @importFrom xml2 write_xml
 #' @seealso \url{https://fbinter.stadt-berlin.de/fb/berlin/service_intern.jsp?id=s_wfs_alkis_bezirk@@senstadt&type=WFS}
@@ -21,20 +21,18 @@
 #' berlin_bezirke <- kwb.fisbroker::read_wfs(dataset_id = "s_wfs_alkis_bezirk")
 read_wfs <- function(dataset_id = "s_wfs_alkis_bezirk",
                      service_version = "2.0.0",
-                     srs_name = "EPSG:4258",
+                     srs = "EPSG:258332",
+                     encoding = "UTF-8",
                      debug = TRUE) {
+  
+  stopifnot(service_version %in% c("1.0.0", "1.1.0", "2.0.0"))
+  stopifnot(srs %in% paste0("EPSG:", c(25833, 4258)))
+  
+  service_type <- "WFS"
   fisbroker_urls <- get_urls()
 
-  ## not used yet. Provides metadata for WFS dataset
-  url_docu <-
-    sprintf(
-      "%s/fb/berlin/service_intern.jsp?id=%s@senstadt&type=WFS",
-      fisbroker_urls$base,
-      dataset_id
-    )
-  #browseURL(url_docu)
-
-  msg <- sprintf("Importing WFS dataset_id '%s' from FIS-Broker",
+  msg <- sprintf("Importing %s dataset_id '%s' from FIS-Broker",
+                 service_type,
                  dataset_id)
   kwb.utils::catAndRun(messageText = msg,
                        expr = {
@@ -43,36 +41,31 @@ read_wfs <- function(dataset_id = "s_wfs_alkis_bezirk",
                          } else {
                            NULL
                          }
+                         
+                         url_dataset  <- sprintf("%s/%s",
+                                                 fisbroker_urls$wfs,
+                                                 dataset_id)
 
-
-
-                         url_wfs_dataset  <- sprintf("%s/%s",
-                                                     fisbroker_urls$wfs,
-                                                     dataset_id)
-
-                         wfs_dataset_client <- ows4R::WFSClient$new(url_wfs_dataset ,
-                                                                    serviceVersion = service_version,
-                                                                    logger = logger)
-
-                         dataset <- wfs_dataset_client$getFeatureTypes(pretty = TRUE)
-
-                         url <- httr::parse_url(url_wfs_dataset)
+                         url <- httr::parse_url(url_dataset)
 
                          url$query <- list(
-                           service = "wfs",
+                           service = service_type,
                            version = service_version,
                            request = "GetFeature",
-                           typenames = dataset$name,
-                           srsName = srs_name
+                           typenames = sprintf("fis:%s", dataset_id),
+                           srs = srs_name
                          )
 
                          request <- httr::build_url(url)
-
-                         # Works
+                         
                          response <- httr::GET(request)
-                         content <- httr::content(response)
-
-
+                         
+                         if(httr::status_code(response)!="200") {
+                           stop(sprintf("Request '%s' failed", request))
+                         }
+                         
+                         content <- httr::content(response, encoding = encoding)
+                         
                          temp_file <-
                            fs::path_join(c(tempdir(), paste0(basename(tempfile(
                            )), ".xml")))
