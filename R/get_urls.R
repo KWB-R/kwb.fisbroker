ee5fa2bed6ea2b93960f2537febcea22d7e32d64
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-24 20:05:21 +0200

    Allow to pass placeholder values

--- R/get_urls.R
+++ R/get_urls.R
@@ -1,5 +1,6 @@
 #' Get Urls
 #'
+#' @param \dots further arguments passed to \code{\link[kwb.utils]{resolve}}
 #' @return return `base` and `wfs` urls of FIS-Broker
 #' @export
 #' @importFrom kwb.utils resolve
@@ -7,12 +8,14 @@
 #' fisbroker_urls <- get_urls()
 #' fisbroker_urls
 #'
-get_urls <- function() {
-urls_list <- list(
-  base = "https://fbinter.stadt-berlin.de",
-  wfs = "<base>/fb/wfs/data/senstadt"
-)
-
-kwb.utils::resolve(urls_list)
+get_urls <- function(...) {
+  
+  urls_list <- list(
+    base = "https://fbinter.stadt-berlin.de",
+    do = "<base>/fb/gisbroker.do",
+    wfs = "<base>/fb/wfs/data/senstadt",
+    meta = "<base>/fb/berlin/service_intern.jsp?id=<id>@senstadt&type=<type>"
+  )
+  
+  kwb.utils::resolve(urls_list, ...)
 }
-
