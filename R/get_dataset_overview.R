b249fa9dc45f3328ac9ddeddea5cdebd32cacffe
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-25 10:27:33 +0200

    Clean get_dataset_overview()
    - use and reuse new helper fun to_id_name()
    - extract columns "cmd", "type", "identifier"
      from hrefs
    - do not include the full hrefs in the output.
      The idea is to have a function that constructs
      the full hrefs from the parameters "cmd",
      "type", "identifier"

--- R/get_dataset_overview.R
+++ R/get_dataset_overview.R
@@ -16,96 +16,79 @@
 #'   with a placeholder} 
 #' } 
 #' @export
-#' @importFrom dplyr arrange bind_rows left_join full_join relocate
-#' @importFrom httr content GET 
+#' @importFrom dplyr arrange bind_rows full_join left_join relocate
+#' @importFrom httr GET content 
 #' @importFrom rvest html_element read_html
 #' @importFrom stats setNames
 #' @importFrom tidyr fill
 #' @examples
 #' fb_dataset_overview <- kwb.fisbroker::get_dataset_overview()
+#' # View(fb_dataset_overview)
 get_dataset_overview <- function(dbg = TRUE)
 {
+  #kwb.utils::assignPackageObjects("kwb.fisbroker")
+  #dbg <- TRUE
+  #`%>%` <- magrittr::`%>%`
+  
   session_id <- login_to_fis_broker(dbg = dbg)
   
-  tabelle <- session_id %>%
+  table_rows <- session_id %>%
     compose_fis_broker_url(cmd = "navigationFrameResult") %>%
     get_html_as_text(dbg = dbg) %>%
     rvest::read_html() %>% 
-    rvest::html_element(css = "table.nav_tabelle") 
+    rvest::html_element(css = "table.nav_tabelle") %>%
+    rvest::html_elements(css = "tr")
   
-  x <- tabelle %>% 
-    rvest::html_elements(css = "tr") 
-  
-  is_category <- stringr::str_detect(as.character(x), "class=\"kategorie\"")
-  
-  categories_df <- tibble::tibble(
-    idx = which(is_category),
-    category_id = seq_len(length(.data$idx)), 
-    category_name = rvest::html_text(x[.data$idx])
+  is_category <- stringr::str_detect(
+    as.character(table_rows), 
+    "class=\"kategorie\""
   )
   
-  is_dataset <- !is_category
+  to_id_name <- function(is_selected, prefix) {
+    do.call(tibble::tibble, args = stats::setNames(
+      nm = c("idx", paste0(prefix, c("id", "name"))),
+      object = list(
+        which(is_selected),
+        seq_len(sum(is_selected)), 
+        rvest::html_text(table_rows[is_selected])
+      )
+    ))
+  }
   
-  datasets_df <- tibble::tibble(
-    idx = which(is_dataset), 
-    dataset_id = seq_len(length(.data$idx)), 
-    dataset_name_raw = rvest::html_text(x[.data$idx])
-  )
+  categories <- to_id_name(is_category, prefix = "category_")
+  datasets <- to_id_name(!is_category, prefix = "dataset_")
   
-  datasets_text_list <- lapply(datasets_df$idx, function(index) {
+  extract_from_row <- function(index) {
+    #index <- 4L
+    elements <- rvest::html_elements(table_rows[index], css = "a.standard")
     
-    elements <- x[index] %>%  
-      rvest::html_elements(css = "a.standard")
+    if (length(elements) == 0L) {
+      stop("no element found")
+    }
     
     elements_text <- rvest::html_text(elements)
     elements_href <- rvest::html_attr(elements, "href") 
     
-    elements_href <- stringr::str_replace(
-      elements_href, 
-      get_session_id(elements_href), 
-      "<jsessionid>"
-    )
+    pattern <- "\\?(cmd=([^&]+)&(type=([^&]+)&)?m?id=([^']+))"
+    elements <- c(cmd = 2L, type = 4L, identifier = 5L)
     
-    n_elements <- length(elements)
-    
-    if (n_elements == 0L) {
-      stop("no element found")
-    }
-    
-    dataset_name <- elements_text[1L]
-    href <- elements_href[1L]
-    dataset_mid <- get_mid(href)
-    
-    if (n_elements == 1L) {
-      return (tibble::tibble(
-        dataset_name = dataset_name, 
-        dataset_mid = dataset_mid,
-        dataset_name_href = href
-      ))
-    } 
-    
-    tibble::tibble(
-      dataset_name = dataset_name, 
-      dataset_mid = dataset_mid,
-      dataset_name_href = href,
-      dataset_type = elements_text[-1L],
-      dataset_type_href = elements_href[-1L]
-    )
-  })
+    kwb.utils::extractSubstring(pattern, elements_href, elements) %>%
+      cbind(dataset_name = elements_text[1L]) %>%
+      kwb.utils::moveColumnsToFront("dataset_name")
+  }
   
-  names(datasets_text_list) <- datasets_df$idx
-  
-  fb_datasets <- dplyr::bind_rows(datasets_text_list, .id = "idx") %>%
-    dplyr::mutate(idx = as.integer(.data$idx))
-  
-  fb_datasets <- categories_df %>%  
-    dplyr::full_join(fb_datasets, by = "idx") %>% 
+  lapply(datasets$idx, extract_from_row) %>%
+    stats::setNames(datasets$idx) %>%
+    dplyr::bind_rows(.id = "idx") %>%
+    dplyr::mutate(idx = as.integer(.data$idx)) %>%  
+    dplyr::full_join(categories, by = "idx") %>% 
     dplyr::arrange(.data$idx) %>% 
     tidyr::fill(.data$category_id, .data$category_name) %>% 
     dplyr::filter(!is.na(.data$dataset_name)) %>%  
-    dplyr::left_join(datasets_df[,c("idx", "dataset_id")], by = "idx") %>% 
+    dplyr::left_join(datasets[, c("idx", "dataset_id")], by = "idx") %>% 
     dplyr::select(- .data$idx) %>% 
-    dplyr::relocate(.data$dataset_id, .before = .data$dataset_name)
-  
-  structure(fb_datasets, session_id = session_id)
+    columns_to_factor(c("category_name", "cmd", "type")) %>%
+    kwb.utils::moveColumnsToFront(c("dataset_id", "dataset_name")) %>%
+    kwb.utils::moveColumnsToFront(c("category_id", "category_name")) %>%
+    structure(session_id = session_id)
 }
