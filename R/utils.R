398d02cac0cbc8e7835fbd6dedfba8529fd051da
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-09-25 10:17:35 +0200

    Add utility functions
    - columns_to_factor()
    - httr_get_or_fail()
    - to_query_string()
    - to_url()
    - write_temp_xml_file()

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,10 @@
+# columns_to_factor ------------------------------------------------------------
+columns_to_factor <- function(data, columns)
+{
+  data[columns] <- lapply(data[columns], kwb.utils::toFactor)
+  data
+}
+                              
 # commentlines -----------------------------------------------------------------
 commentlines <- function()
 {
@@ -11,15 +18,61 @@ commentlines <- function()
 #' @importFrom kwb.utils catAndRun
 get_html_as_text <- function(url, dbg = TRUE)
 {
-  kwb.utils::catAndRun(
-    sprintf("Getting HTML text from '%s'", url),
-    httr::content(httr::GET(url), as = "text"),
-    dbg = dbg
-  )
+  msg <- sprintf("Getting HTML text from '%s'", kwb.utils::shorten(url, 50L))
+  
+  kwb.utils::catAndRun(msg, dbg = dbg, expr = {
+    url %>%
+      httr_get_or_fail() %>%
+      httr::content(as = "text")
+  })
+}
+
+# httr_get_or_fail -------------------------------------------------------------
+#' @importFrom httr GET status_code
+httr_get_or_fail <- function(url)
+{
+  response <- httr::GET(url)
+  
+  if (httr::status_code(response) != "200") {
+    stop(sprintf("Request '%s' failed", url))
+  }
+
+  response
 }
 
 # replace_non_breaking_spaces --------------------------------------------------
 replace_non_breaking_spaces <- function(x)
 {
   gsub(pattern = "\ua0", replacement = " ", x)
+}
+
+# to_query_string --------------------------------------------------------------
+to_query_string <- function(...)
+{
+  x <- list(...)
+  
+  if (length(x) == 0L) {
+    return("")
+  }
+  
+  paste0(names(x), "=", as.character(x), collapse = "&")
+}
+
+# to_url -----------------------------------------------------------------------
+to_url <- function(x)
+{
+  kwb.utils::addClass(x, "url")  
+}
+
+# write_temp_xml_file ----------------------------------------------------------
+write_temp_xml_file <- function(content)
+{
+  temp_file <-   fs::path_join(c(
+    tempdir(), 
+    paste0(basename(tempfile()), ".xml")
+  ))
+  
+  xml2::write_xml(content, temp_file)
+
+  temp_file  
 }
